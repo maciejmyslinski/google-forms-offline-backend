486304bc09520057c76d1e2f91730112299e9d38
Maciej Myśliński <maciej.myslinski@netguru.pl>
2017-08-16 23:35:53 +0200

    Write a function that save form description to the database

--- functionsES6/index.js
+++ functionsES6/index.js
@@ -1,13 +1,20 @@
-import { https } from 'firebase-functions';
+import firebase from 'firebase-functions';
 import express from 'express';
 import bodyParser from 'body-parser';
+import admin from 'firebase-admin';
 
+admin.initializeApp(firebase.config().firebase);
 const app = express();
 const v1 = express.Router();
 app.use(bodyParser.json());
 app.use('/v1', v1);
 v1.post('/form-description', (request, response) => {
+  if (!request.body.id) {
+    response.status(400).end();
+  }
+  const formsRef = admin.database().ref('forms');
+  formsRef.child(request.id).set(request.body);
   response.status(200).end();
 });
 
-export default https.onRequest(app);
+export default firebase.https.onRequest(app);
